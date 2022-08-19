e3180c6befe3e262c33d5bd510b31bcab74b14c5
Graham Christensen <graham@grahamc.com>
2022-08-19 15:57:42 -0400

    Bootstrap: wait for network

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,10 @@ set -eux
 
 cd "$(mktemp -d)"
 
+while ! ping -c1 gist.githubusercontent.com; do
+	sleep 1
+done
+
 curl -L -o apply.sh https://gist.githubusercontent.com/grahamc/8694fac95ff865a468c94d605c4b0a66/raw/apply.sh
 chmod +x ./apply.sh
 ./apply.sh "my-buildkite-token"
