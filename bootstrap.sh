da919ed5af3fb29b49c306b1d17ad78ee4abcb68
Graham Christensen <graham@grahamc.com>
2022-08-19 15:58:04 -0400

    bootstrap: show tailscale token

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,5 +10,5 @@ done
 
 curl -L -o apply.sh https://gist.githubusercontent.com/grahamc/8694fac95ff865a468c94d605c4b0a66/raw/apply.sh
 chmod +x ./apply.sh
-./apply.sh "my-buildkite-token"
+./apply.sh "my-buildkite-token" "my-tailscale-token"
 
