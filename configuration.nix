2980cad16209f1c30d46e25baf928236086e5836
Graham Christensen <graham@grahamc.com>
2022-10-12 14:11:02 -0400

    Launch tailscaled in ephemeral mode

--- configuration.nix
+++ configuration.nix
@@ -97,7 +97,7 @@
 
   launchd.daemons.tailscaled = {
     script = ''
-      exec ${pkgs.tailscale}/bin/tailscaled
+      exec ${pkgs.tailscale}/bin/tailscaled –state=mem:
     '';
 
     serviceConfig.KeepAlive = true;
